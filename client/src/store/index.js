6a63e389f8335512495897e4852f7bcdaa367538
Dakota Logan <xeliusone@gmail.com>
2020-01-07 16:50:13 -0700

    Functional oriented store functions, please no more.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,84 +4,145 @@ import Axios from 'axios'
 import router from '../router/index'
 import AuthService from '../AuthService'
 
-Vue.use(Vuex)
+Vue.use (Vuex);
 
 //Allows axios to work locally or live
-let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+let base = window.location.host.includes ('localhost:8080') ? '//localhost:3000/' : '/'
 
-let api = Axios.create({
-  baseURL: base + "api/",
-  timeout: 3000,
-  withCredentials: true
+let api = Axios.create ({
+	baseURL: base + "api/",
+	timeout: 3000,
+	withCredentials: true
 })
 
-export default new Vuex.Store({
-  state: {
-    user: {},
-    boards: [],
-    activeBoard: {}
-  },
-  mutations: {
-    setUser(state, user) {
-      state.user = user
-    },
-    setBoards(state, boards) {
-      state.boards = boards
-    }
-  },
-  actions: {
-    //#region -- AUTH STUFF --
-    async register({ commit, dispatch }, creds) {
-      try {
-        let user = await AuthService.Register(creds)
-        commit('setUser', user)
-        router.push({ name: "boards" })
-      } catch (e) {
-        console.warn(e.message)
-      }
-    },
-    async login({ commit, dispatch }, creds) {
-      try {
-        let user = await AuthService.Login(creds)
-        commit('setUser', user)
-        router.push({ name: "boards" })
-      } catch (e) {
-        console.warn(e.message)
-      }
-    },
-    async logout({ commit, dispatch }) {
-      try {
-        let success = await AuthService.Logout()
-        if (!success) { }
-        commit('resetState')
-        router.push({ name: "login" })
-      } catch (e) {
-        console.warn(e.message)
-      }
-    },
-    //#endregion
-
-
-    //#region -- BOARDS --
-    getBoards({ commit, dispatch }) {
-      api.get('boards')
-        .then(res => {
-          commit('setBoards', res.data)
-        })
-    },
-    addBoard({ commit, dispatch }, boardData) {
-      api.post('boards', boardData)
-        .then(serverBoard => {
-          dispatch('getBoards')
-        })
-    }
-    //#endregion
-
-
-    //#region -- LISTS --
-
-
-
-    //#endregion
-  }
+export default new Vuex.Store ({
+	state: {
+		user: {},
+		boards: [],
+		lists: [],
+		tasks: [],
+		comments: [],
+		activeBoard: {}
+	},
+	mutations: {
+		setAll (state, payload) {
+			state[payload.address] = payload.data;
+		},
+		
+		setOne (state, payload) {
+			state[payload.address] = payload.data;
+		},
+		
+		addOne (state, payload) {
+			state[payload.address].push (payload.data);
+		},
+		
+		removeOne (state, payload) {
+			state[payload.address] = state[payload.address].filter (
+			cur => cur.id === payload.data
+			)
+		},
+		
+		resetState (state) {
+			state = {
+				user: {},
+				boards: [],
+				activeBoard: {}
+			}
+		},
+		
+	},
+	actions: {
+		//#region -- AUTH STUFF --
+		async register ({commit, dispatch}, creds) {
+			try {
+				let user = await AuthService.Register (creds);
+				
+				commit ('setOne', {address: 'user', data: user});
+				
+				router.push ({name: "boards"})
+			} catch (e) {
+				console.warn (e.message)
+			}
+		},
+		async login ({commit, dispatch}, creds) {
+			try {
+				let user = await AuthService.Login (creds);
+				commit ('setOne', {address: 'user', data: user});
+				router.push ({name: "boards"})
+			} catch (e) {
+				console.warn (e.message)
+			}
+		},
+		async logout ({commit, dispatch}) {
+			try {
+				let success = await AuthService.Logout ();
+				if (!success) {
+				}
+				commit ('resetState');
+				router.push ({name: "login"})
+			} catch (e) {
+				console.warn (e.message)
+			}
+		},
+		//#endregion
+		
+		getAll ({commit}, address) {
+			api
+			.get ('' + address)
+			.then (res =>
+			commit ('setAll', {
+				address: `${address}`,
+				data: res.data
+			})
+			)
+			.catch (e => console.error (e, e.message));
+		},
+		
+		getOne ({commit}, payload) {
+			api
+			.get ('' + payload.address)
+			.then (res => {
+				commit (payload.commit, {
+					address: `${address}`,
+					data: res.data
+				})
+			})
+			.catch (e => console.error (e, e.message));
+		},
+		
+		create ({commit}, payload) {
+			api
+			.post ('' + payload.address, payload.data)
+			.then (res => {
+				commit (payload.commit, {
+					address: `${address}`,
+					data: res.data
+				})
+			})
+			.catch (e => console.error (e, e.message));
+		},
+		
+		edit ({commit}, payload) {
+			api
+			.put ('' + payload.address, payload.data)
+			.then (res => commit (payload.commit, {
+				address: payload.address,
+				data: res.data
+			}))
+			.catch (e => console.error (e));
+		},
+		
+		delete ({commit}, payload) {
+			api
+			.delete ('' + payload.address + payload.id)
+			.then (res => {
+				commit (payload.commit, {
+					address: `${payload.address}`,
+					data: payload.id
+				})
+			})
+			.catch (e => console.error (e, e.message));
+		}
+	}
 })
